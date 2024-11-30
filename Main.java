c8a4b76280a3f454eb189442dfd464314e012953
vijaybalakrishna123 <vijaybalakrishna123@gmail.com>
2024-11-30 23:13:10 +0530

    Add files via upload
    Program to print int,string,char

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("Enter input");
+        Scanner scan = new Scanner(System.in);
+         int num = scan.nextInt();
+    System.out.println(num);
+    
+    String str  = scan.next();
+    System.out.println(str);
+    
+    char c = scan.next().charAt(0);
+    System.out.println(c);
+    
+    }
+}
