2c329961c129f7e6365cf147d634ddf5b29ed4f0
vijaybalakrishna123 <vijaybalakrishna123@gmail.com>
2024-12-09 18:26:54 +0530

    Update reverse stars pattern.java

--- reverse stars pattern.java
+++ reverse stars pattern.java
@@ -1,3 +1,22 @@
+/*Write a Program which takes input n and prints the below pattern
+
+Sample 1:
+
+Input:
+
+n = 4
+
+Expected output:
+
+****
+
+***
+
+**
+
+*
+
+*/
 class Main {
     public static void main(String[] args) {
         int n=5;
